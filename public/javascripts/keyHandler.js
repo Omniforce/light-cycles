88fda6609d9de6c987c55767be30ec8969153046
ltrowel-cengage <liam.trowel@cengage.com>
2014-05-22 11:51:52 -0400

    added keyHandling

---
+++ public/javascripts/keyHandler.js
@@ -0,0 +1,24 @@
+$(function(){
+	window.addEventListener('keydown', keyDown);
+
+	function keyDown(){
+		var keyData;
+		if(event.keyCode === 37){
+			keyData = {key:'left'};
+		}
+		else if(event.keyCode === 38){
+			keyData = {key:'up'};
+		}
+		else if(event.keyCode === 39){
+			keyData = {key:'right'};
+		}
+		else if(event.keyCode === 40){
+			keyData = {key:'down'};
+		}
+		else{
+			return;
+		}
+		socket.emit('keyPress', JSON.stringify(keyData));
+	}
+
+})
