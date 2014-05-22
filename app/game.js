3af33c7a4a83fa1e336014f9219168054e0cf131
ltrowel-cengage <liam.trowel@cengage.com>
2014-05-22 14:10:29 -0400

    Stops player from exiting the screen

--- app/game.js
+++ app/game.js
@@ -4,6 +4,7 @@ function Game() {
 
 	this.tick = function() {
 		this.player1.move();
+		this.playerTouchingBorder(this.player1);
 	}
 
 	this.jsonifyGame = function() {
@@ -12,6 +13,13 @@ function Game() {
 			y: this.player1.y,
 			color: this.player1.color
 		}
+	}
+
+	this.playerTouchingBorder = function(player) {
+		if(player.x > 119){player.x = 119; }
+		if(player.x < 0){player.x = 0; }
+		if(player.y > 119){player.y = 119; }
+		if(player.y < 0){player.y = 0; }
 	}
 }
 
